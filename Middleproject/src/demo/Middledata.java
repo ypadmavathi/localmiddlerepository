316e68089b5414287f5d21e183fcf3c7bc6fed57
padmavathi.y <padmavathi.y@DS-9121AA344592.HCLT.CORP.HCL.IN>
2017-08-28 17:24:47 +0530

    added to output statement

---
+++ Middleproject/src/demo/Middledata.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class Middledata {
+
+	public static void main(String[] args) {
+		displaymiddledata();
+
+	}
+
+	private static void displaymiddledata() {
+        System.out.println("Hello");
+		
+	}
+
+}
